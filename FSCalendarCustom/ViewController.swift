b91dce21884f4fd8f200a331757ee1ed22e82a72
Alex <kjj9647@gmail.com>
2019-07-14 17:27:50 +0900

    select several days and deselect function added

--- FSCalendarCustom/ViewController.swift
+++ FSCalendarCustom/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - UI Properties
     let containerView = UIView()
     let topTextLabel = UILabel()
     var weekdayLabel = UILabel()
@@ -20,8 +21,12 @@ class ViewController: UIViewController {
     
     private weak var calendar: FSCalendar!
     
+    // MARK: - Properties
     let currentDate = Date()
     let dateFormatter = DateFormatter()
+    
+    var selectDatesArray = [Date]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -140,6 +145,8 @@ class ViewController: UIViewController {
         calendar.appearance.titlePlaceholderColor = #colorLiteral(red: 0.7327679992, green: 0.7284137607, blue: 0.7361161113, alpha: 0.4171660959)
         calendar.appearance.selectionColor = UIColor(red:0.09, green:0.51, blue:0.54, alpha:1.0)
         
+        
+        
         //        calendar.appearance.titleColors
         calendar.swipeToChooseGesture.isEnabled = true
         
@@ -148,13 +155,23 @@ class ViewController: UIViewController {
         self.calendar = calendar
     }
     
+    private func lastDayOfMonth(date: Date) -> Date {
+        let calendar = Calendar.current
+        var components = calendar.dateComponents([.year, .month, .day], from: date)
+        let range = calendar.range(of: .day, in: .month, for: date)!
+        components.day = range.upperBound - 1
+        return calendar.date(from: components)!
+    }
+    
     
 }
 
 extension ViewController: FSCalendarDelegate, FSCalendarDataSource {
     func calendar(_ calendar: FSCalendar, shouldSelect date: Date, at monthPosition: FSCalendarMonthPosition) -> Bool {
+        print("--------------------------[ShouldSelect]--------------------------")
         dateFormatter.dateFormat = "yyyy-MM-dd hh:mm"
         let selectDateString = dateFormatter.string(from: date)
+        print("monthPosition: ", monthPosition)
         print("selectDate: ", selectDateString)
         
         //        calendar.cell(for: date, at: monthPosition)?
@@ -164,6 +181,15 @@ extension ViewController: FSCalendarDelegate, FSCalendarDataSource {
         let selectDay = dateFormatter.string(from: date)
         print("currentDay: ", currentDay)
         print("selectDay: ", selectDay)
+        
+        // =================================== ===================================
+        
+//        let cell = calendar.cell(for: date.addingTimeInterval(3600 * 24), at: FSCalendarMonthPosition.current)
+//        let nextDay = calendar.date(for: cell!)
+//        let nextDayString = dateFormatter.string(from: nextDay!)
+        //        print("** Calendar Cell: ", nextDayString)
+
+      
         
         if currentDay == selectDay {
             return false
@@ -180,15 +206,75 @@ extension ViewController: FSCalendarDelegate, FSCalendarDataSource {
     func maximumDate(for calendar: FSCalendar) -> Date {
         let laterDate = Calendar.current.date(byAdding: .month, value: 2, to: currentDate)!
         //        return currentDate.addingTimeInterval(3600 * 24 * 30)
-        return laterDate
+        let lastDayMonth = lastDayOfMonth(date: laterDate)
+        return lastDayMonth
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("--------------------------[DidSelect]--------------------------")
+        let nextDay2 = Calendar.current.date(byAdding: .day, value: 1, to: date)!
+        let nextDayString2 = dateFormatter.string(from: nextDay2)
+        print("** nextDay: ", nextDayString2)
+        
+//        let nextDayCell = calendar.cell(for: nextDay2, at: monthPosition)
+//        calendar.select(nextDay2)
+        switch selectDatesArray.count {
+        case 0:
+            print("currentCount: ", selectDatesArray.count)
+            selectDatesArray.append(date)
+            print(selectDatesArray)
+        case 1:
+            // 이미 1개가 선택되어있을때
+            print("currentCount: ", selectDatesArray.count)
+            selectDatesArray.append(date)
+            selectDatesArray.sort()
+            
+            let timeGap = selectDatesArray[1].timeIntervalSince(selectDatesArray[0])
+            let oneDayValue: TimeInterval = 3600 * 24
+            let daysGap = Int(timeGap / oneDayValue)
+            
+            for i in 1...daysGap {
+                let day = Calendar.current.date(byAdding: .day, value: i, to: selectDatesArray[0])
+                selectDatesArray.append(day!)
+                calendar.select(day)
+            }
+            selectDatesArray.removeLast()
+            selectDatesArray.sort()
+            print(selectDatesArray)
+        case 2...:
+            // 눌렀을때 이미 2개이상이 선택되있는 상황일때
+            print("currentCount: ", selectDatesArray.count)
+            selectDatesArray.forEach{
+                calendar.deselect($0)
+            }
+            selectDatesArray.removeAll()
+            selectDatesArray.append(date)
+            print(selectDatesArray)
+        default : break
+        }
+        
+        
+        
+    }
+    
+    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("--------------------------[DidDeselect]--------------------------")
+        if selectDatesArray.contains(date) {
+            selectDatesArray.forEach{
+                calendar.deselect($0)
+            }
+            selectDatesArray.removeAll()
+            calendar.select(date)
+            selectDatesArray.append(date)
+        }
+        print(selectDatesArray)
+        
+    }
+
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        print("--------------------------[PageDidChange]--------------------------")
     }
     
     
     
-    
-    
-    
-    //    func maximumDate(for calendar: FSCalendar) -> Date {
-    //        <#code#>
-    //    }
 }
